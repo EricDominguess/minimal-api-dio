1771f563842eba3f5c4d7560cb44da9f16032fb2
Eric Domingues <ericdom729@gmail.com>
2025-11-03 23:15:23 -0300

    banco de dados configurado

--- Infraestrutura/Db/Dbcontexto.cs
+++ Infraestrutura/Db/Dbcontexto.cs
@@ -1,7 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Entidades;
+
 namespace MinimalApi.Infraestrutura.Db;
 
 // CONTEXTO DO BANCO DE DADOS
-public class Dbcontexto
+public class Dbcontexto : DbContext
 {
-    
+  private readonly IConfiguration _configurationAppSettings;
+  public Dbcontexto(IConfiguration configurationAppSettings)
+  {
+    _configurationAppSettings = configurationAppSettings;
+  }
+
+  public DbSet<Administrador> Administradores { get; set; } = default!;
+
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  {
+    if (!optionsBuilder.IsConfigured)
+    {
+      var stringConexao = _configurationAppSettings.GetConnectionString("mysql")?.ToString();
+
+      if (!string.IsNullOrEmpty(stringConexao))
+      {
+        optionsBuilder.UseMySql(stringConexao,
+          ServerVersion.AutoDetect(stringConexao));
+      }
+    }
+  }
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
+using MinimalApi.Infraestrutura.Db;
+using MinimalApi.DTOs;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<Dbcontexto>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
+// GET SIMPLES PARA TESTE
 app.MapGet("/", () => "Hello World!");
 
 // POST DE LOGIN PARA VALIDAÇÃO DE USUÁRIO
-app.MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDTO) =>
+app.MapPost("/login", (LoginDTO loginDTO) =>
 {
   if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
   {
