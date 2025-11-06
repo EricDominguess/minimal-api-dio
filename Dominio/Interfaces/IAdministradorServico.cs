88a38104a839f3410f66b9400a06e9688db52b37
Eric Domingues <ericdom729@gmail.com>
2025-11-06 19:00:02 -0300

    criado os endpoints de administrador

---
+++ Dominio/Interfaces/IAdministradorServico.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MinimalApi.Dominio.Entidades;
+using MinimalApi.Dominio.DTOs;
+
+namespace MinimalApi.Dominio.Interfaces
+{
+  public interface IAdministradorServico
+  {
+    Administrador? Login(LoginDTO loginDTO);
+    Administrador Incluir(Administrador administrador);
+    List<Administrador> Todos(int? pagina);
+    Administrador? BuscarPorId(int id);
+      
+  }
+}
